e59ff704100c36f6a7fdf261ba437ed76c26c879
Zebadiah <zebadiahww@gmail.com>
2019-10-09 09:46:53 -0600

    project created

---
+++ RedditStarteriOS29/RedditStarteriOS29/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RedditStarteriOS29
+//
+//  Created by Darin Armstrong on 10/9/19.
+//  Copyright © 2019 Darin Armstrong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ RedditStarteriOS29/RedditStarteriOS29/Resources/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  RedditStarteriOS29
+//
+//  Created by Darin Armstrong on 10/9/19.
+//  Copyright © 2019 Darin Armstrong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
